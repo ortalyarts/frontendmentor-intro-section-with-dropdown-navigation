b39e1af1813b488d783ed8f207f583bdce338633
ortalyarts <148264882+ortalyarts@users.noreply.github.com>
2024-03-22 14:11:44 +0100

    Accessibility - big fix

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const menuIcon = document.querySelector('#menu-icon');
-const closeIcon = document.querySelector('#close-icon');
+
 const menuContainer = document.querySelector('#top-nav-container');
 const menuUl = document.querySelector('#nav-ul');
 const userActionsUl = document.querySelector('#user-actions');
@@ -7,7 +7,7 @@ const userActionsUl = document.querySelector('#user-actions');
 const login = document.querySelector('#login');
 const register = document.querySelector('#register');
 
-// move ul#user-actions under main ul
+// move ul#user-actions under main ul - irrelevant for accessibility
 (function() {
   window.onresize = displayWindowSize;
   window.onload = displayWindowSize;
@@ -26,20 +26,79 @@ const register = document.querySelector('#register');
   };
 })();
 
-
-menuIcon.addEventListener('click', ()=>{
-  menuContainer.style.right='0%';
-  menuUl.style.opacity='1';
-});
-closeIcon.addEventListener('click', ()=>{
+// for Mobile - Toggle open menu
+const closeMainMenu = () => {
   menuContainer.style.right='-100%';
   menuUl.style.opacity='0';
+  menuIcon.ariaExpanded = "false";
+  menuIcon.focus();
+}
+
+menuIcon.addEventListener('click', ()=>{
+  if(menuIcon.ariaExpanded == 'false'){
+    menuContainer.style.right='0%';
+    menuUl.style.opacity='1';
+    menuIcon.ariaExpanded = "true";
+  } else {
+    closeMainMenu();
+  }
+  
 });
 
 const dropdownLinks = document.querySelectorAll('.dropdown');
 
-dropdownLinks.forEach((elem)=>{
-  elem.addEventListener('click', () =>{
-    elem.classList.toggle('open');
+const toggleSubMenu = (elem) => {
+  if(elem.classList.contains('open')){
+    closeSubMenu(elem);
+    
+  } else {
+    elem.classList.add('open');
+    elem.ariaExpanded = "true";
+    elem.nextElementSibling.ariaHidden ='false'; // show sub menu Ul container
+    // show sub menu links
+    const subMennu = elem.nextElementSibling.querySelectorAll('a');
+    subMennu.forEach((link) => {
+      link.ariaHidden ='false';
+      link.tabIndex = '0'
+    })
+    elem.nextElementSibling.querySelector('a').focus(); // focus on the first linkfrom sub menu
+  }
+}
+
+const closeSubMenu = (elem) => {
+  elem.classList.remove('open');
+  elem.ariaExpanded = "false";
+  elem.nextElementSibling.ariaHidden = 'true'; // hide sub menu Ul container
+  // hide sub menu links
+  const subMennu = elem.nextElementSibling.querySelectorAll('a');
+  subMennu.forEach((link) => {
+    link.ariaHidden ='true';
+    link.tabIndex = '-1'
   })
-})
+}
+
+// Close sublinks if clicked elsewere
+document.addEventListener("click", (event) => {
+
+  dropdownLinks.forEach((elem)=>{
+    if (elem.contains(event.target)) {
+      toggleSubMenu(elem);
+    } else {
+      closeSubMenu(elem);
+    }
+  })
+});
+
+// Close sublinks if ESC is pressed
+document.addEventListener('keyup', function(e) {
+  // console.log(e.code);
+  if (e.key == 'Escape') {
+    const openLink = document.querySelector('.open');
+    if(openLink){
+      closeSubMenu(openLink);
+      openLink.focus();
+    } else if(menuIcon.ariaExpanded == 'true'){
+      closeMainMenu();
+    }
+  }
+});
